368b7fad62ec0ec5a74a95c8a9c02b46a9e26528
glorysaladin <905040963@qq.com>
2023-12-08 01:30:36 +0800

    update

--- plugins/movie/read_pickle.py
+++ plugins/movie/read_pickle.py
@@ -2,13 +2,28 @@ import os
 
 import pickle
 
-os.getcwd()
+def read_pickle(path):
+    with open(path, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+def write_pickle(path, content):
+    with open(path, "wb") as f:
+        pickle.dump(content, f)
+    return True
 
 #os.chdir("文件所在的路径")
 
-#file = open("moviename_whitelist.pkl","rb")
-file = open("user_datas.pkl","rb")
+#file = open("user_datas.pkl","rb")
 
-content = pickle.load(file)
+#content = pickle.load(file)
 
-print(content)
+user_datas = read_pickle("user_datas.pkl")
+for key in user_datas:
+    if not user_datas[key]["is_pay_user"]:
+        if user_datas[key]['limit'] > 1:
+            user_datas[key]['limit'] = 0
+       
+    #print(key, user_datas[key])
+#
+write_pickle("user_datas.pkl", user_datas)
