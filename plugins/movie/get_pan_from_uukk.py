656dbcdadea5594aadac82dae869f1d7f44ed5db
glorysaladin <905040963@qq.com>
2024-07-22 00:58:19 +0800

    update

--- plugins/movie/get_pan_from_uukk.py
+++ plugins/movie/get_pan_from_uukk.py
@@ -10,10 +10,9 @@ def good_match(s1, s2):
        return True 
     return False
 
-#COOKIE="Hm_lvt_cce07f87930c14786d9eced9c08d0e89=1700320320,1700750079,1701444675; Hm_lpvt_cce07f87930c14786d9eced9c08d0e89=1701964941"
-COOKIE="Hm_lvt_0f5b4479829961ae8a3cb0e80ec93808=1711593533; Hm_lpvt_0f5b4479829961ae8a3cb0e80ec93808=1711593543"
+COOKIE="Hm_lvt_cce07f87930c14786d9eced9c08d0e89=1714149361,1715772698; Hm_lvt_0f5b4479829961ae8a3cb0e80ec93808=1720437132,1720530560,1720695559,1720963126; __51cke__=; __tins__21897559=%7B%22sid%22%3A%201721580186807%2C%20%22vd%22%3A%203%2C%20%22expires%22%3A%201721582100028%7D; __51laig__=19"
 #URLS=["http://22006.cn/v/api/getJuzi", "http://22006.cn/v/api/getDyfx", "http://22006.cn/v/api/getTTZJB"]
-URLS=["http://cc.yatspa.com/v/api/getJuzi", "http://cc.yatspa.com/v/api/getTTZJB", "http://cc.yatspa.com/v/api/getDyfx"]
+URLS=["http://uukk6.cn/v/api/getJuzi", "http://uukk6.cn/v/api/getDyfx", "http://uukk6.cn/v/api/getTTZJB"]
 
 def get_from_uukk(query, is_pay_user):
     rets = []
@@ -21,6 +20,7 @@ def get_from_uukk(query, is_pay_user):
         if not is_pay_user and len(rets) > 0:
             break
         rets.extend(search(query, URL))
+    print("uukk_rets=", rets)
     return rets
 
 def search(query, url):
@@ -31,7 +31,13 @@ def search(query, url):
         return_cmd = subprocess.run(shell_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8',shell=True)
         if return_cmd.returncode == 0:
             ret_val = return_cmd.stdout
-            js = json.loads(ret_val)
+            js = {}
+            try:
+                js = json.loads(ret_val)
+            except:
+                print("ret_val=", ret_val)
+                print(traceback.format_exc())
+
             for item in js["list"]:
                 question=item['question']
                 answer=item["answer"]
@@ -45,9 +51,9 @@ def search(query, url):
                     tmp="{}\n{}".format(question, answer)
                     tmp = tmp.replace("\n\n", "\n")
                     rets.append(tmp)
-        #print("*********{}\n{}\n\n".format(url , rets))
+        print("*********{}\n{}\n\n".format(url , rets))
     except:
         print(traceback.format_exc())
     return rets
 
-#print(get_from_uukk("天龙八部", True))
+#print(get_from_uukk("梦想城", True))
